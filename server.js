ce802cb82e1980803a67853153863f0ec6107faf
Daniel <dmolcap@gmail.com>
2021-11-11 20:12:35 +0000

    Add signin endpoint

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+// Mimic the future database
+const database = {
+  users: [
+    {
+      id: 123,
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: 456,
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
 app.get("/", (req, res) => {
   res.send("this is working");
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("Login Success!");
+  } else {
+    res.json("Login Error!");
+  }
 });
 
 app.listen(3000, () => {
