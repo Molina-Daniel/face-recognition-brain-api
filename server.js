343a3af76e3df42df5afad79d212d51e54a15300
Daniel <dmolcap@gmail.com>
2022-02-07 18:25:02 +0100

    Refact register endpoint into different file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt, { hash } from "bcrypt";
 import cors from "cors";
 import knex from "knex";
+import { handleRegister } from "./controllers/register.js";
 
 const db = knex({
   client: "pg",
@@ -50,32 +51,7 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password, saltRounds);
-
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("Unable to register"));
+  handleRegister(req, res, db, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
